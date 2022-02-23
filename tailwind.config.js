603ce19ad3d425a550010cb87c048da61cff3720
Sridhar Chandrasekar <75136330+Sridhar-C-25@users.noreply.github.com>
2022-02-23 18:10:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        dark: "#2B2B2B",
+        Orange: "#E78A00",
+      },
+    },
+  },
+  plugins: [],
+};
